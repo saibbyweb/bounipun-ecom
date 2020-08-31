e8a7a6cee2e233d018d20d351a6ebf1d16a6563a
Suhaib Khan <hello@saibbyweb.com>
2020-08-31 20:47:51 +0530

    graphql arguments crackedt

--- express/models/category.js
+++ express/models/category.js
@@ -1,8 +1,7 @@
 import mongoose from "mongoose";
 let ObjectId = mongoose.Schema.Types.ObjectId;
-import NestedSetPlugin from "../helpers/express/nestedSetMongo";
+import NestedSetPlugin from "./_nestedSet";
 import slugify from "slugify";
-import task from "../helpers/common/task";
 
 const schema = mongoose.Schema({
     name: String,
@@ -26,53 +25,5 @@ const schema = mongoose.Schema({
 schema.plugin(NestedSetPlugin);
 
 const model = mongoose.model('Category', schema);
-
-
-async function updateAll() {
-    
-    const allDocs = await model.find({depth: 11}).populate('parentId', 'slug');
-    console.log(allDocs)
-     allDocs.forEach(async doc => {
-
-         const {response, error} = await task(model.findOneAndUpdate({_id: doc._id}, {slug: doc.parentId.slug + "/" + doc.alias}));
-
-      });
-}
-
-//updateAll();
-
-async function refresh() {
-    const rootNode = model.findOne({ name: "ROOT" })
-    model.rebuildTree(rootNode, 1, () => {
-        /* rootNode.descendants(function (err, data) {
-                console.log(data);
-              }); */
-        console.log('category tree rebuild');
-    });
-}
-//
-//refresh();
-
-async function addData() {
-    await new model({
-        name: "Level4 - About with Sidebar",
-        depth: 4,
-        parentId: "5ee51a4d47e0d61b55049c70",
-        isParent: true
-    }).save();
-}
-//addData();
-
-async function fetch() {
-    const tree = await model.find().sort({lft: 1});
-    tree.forEach(node => {
-        console.log(node.name);
-
-    })
-    //console.log(tree);
-}
-//fetch();
-
-//updateVals();
 
 export default model;
--- express/server.js
+++ express/server.js
@@ -3,8 +3,10 @@ import { ApolloServer, gql } from "apollo-server-express";
 const app = express();
 import mongoose from "mongoose";
 import product from "./models/products";
+import Category from "./models/category";
+import Collection from "./models/collection"
 
-mongoose.connect("mongodb://localhost:27017/mbm-ecom", {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
+mongoose.connect("mongodb://localhost:27017/mbm-ecom", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
 /* TODO: body parser may be required */
 
@@ -18,19 +20,56 @@ const book = {
 }
 
 const typeDefs = gql`
+
+    input Something {
+        model: String,
+        limit: Int,
+        skip:Int
+    }
+
     type Query {
         hello: Book,
         products: [Product]
+        product(slug: String): Product
+        productPagination(data: Something): [Product]
+    }
+
+    input ProductPaginationData {
+        model: String
+        searchTerm: String
+        filterCriteria: String
+        sortByCriteria: String
+        skip: Int
+        limit: Int
     }
 
     type Product {
-        name: String,
+        name: String
         tagline: String
+        alias: String
+        slug: String,
+        category: Category
+        _collection: Collection,
+        variations: [Variation]
+    }
+
+    type Variation {
+        value: String
+        unit: String
+    }
+
+    type Collection {
+        name: String,
+        alias: String
+    }
+
+    type Category {
+        name: String
     }
 
     type Book {
-        name: String,
-        author: String,
+        name: String
+        author: String
         published: Boolean
     }
 `;
@@ -38,14 +77,18 @@ const typeDefs = gql`
 const resolvers = {
     Query: {
         hello: () => book,
-        products: () => product.find()
+        products: () => product.find(),
+        product: async (_, { slug }) => {
+            return product.findOne({ slug }).populate('category').populate('_collection');
+        },
+        productPagination: (_, { data }) => {
+            console.log(data)
+            return product.find()
+        }
     }
 }
 
+const apolloServer = new ApolloServer({ typeDefs, resolvers, playground: true, introspection: true });
+apolloServer.applyMiddleware({ app });
 
-
-
- const apolloServer = new ApolloServer({typeDefs, resolvers, playground:true, introspection: true});
- apolloServer.applyMiddleware({app});
-
-app.listen({port: 3000}, () => console.log(`GraphQL server running on port: 6000 at http://localhost:3000${apolloServer.graphqlPath}`))
+app.listen({ port: 3000 }, () => console.log(`GraphQL server running on port: 6000 at http://localhost:3000${apolloServer.graphqlPath}`))
