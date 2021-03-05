cd5f730ecb1cb8b20fb63a5cf3a68f114c096502
Suhaib Khan <hello@saibbyweb.com>
2021-03-05 17:51:08 +0530

    create card component created

--- pages/My-Account/gift-cards.vue
+++ pages/My-Account/gift-cards.vue
@@ -11,17 +11,33 @@
         <!-- fields -->
         <div class="fields center-col">
             <input class="field" type="text" placeholder="Enter Gift Card No." />
-            <input class="field" type="pin" placeholder="Enter Pin" />
+            <input class="field" type="text" placeholder="Enter Pin" />
         </div>
         <!-- redeem button -->
         <button class="action"> Redeem Card </button>
     </div>
 
-    <!-- create new card -->
-    <div class="create-card center-col">
+    <!-- create new card action-->
+    <div class="create-card-action center-col">
         <p class="desc"> Give your loved ones the gift of choice with
             Bounipun Gift Cards. </p>
         <button class="action"> Create Gift Card > </button>
+    </div>
+
+    <!-- create card -->
+    <div class="create-card center-col">
+        <h3 class="heading"> Create Gift Card </h3>
+        <!-- fields -->
+        <div class="fields center-col">
+            <input class="field" type="text" placeholder="Enter Amount" />
+            <input class="field" type="text" placeholder="Recipient Name" />
+            <input class="field" type="text" placeholder="Mobile Number" />
+            <input class="field" type="text" placeholder="Recipient Email" />
+        </div>
+        <!-- redeem button -->
+        <button class="action"> Proceed to Purchase </button>
+
+        <span class="disclaimer"> The Gift Card cannot be cancelled, refunded or returned. </span>
     </div>
 
 </div>
@@ -35,6 +51,8 @@ export default {
 
 <style lang="scss" scoped>
 .gift-cards {
+
+    /* redeem card */
     .redeem-card {
         box-shadow: 1px 1px 15px rgba(0, 0, 0, 0.16);
         background-color: white;
@@ -44,26 +62,44 @@ export default {
             text-transform: uppercase;
         }
 
-        .fields {
-            width: 100%;
-            margin-bottom: 10px;
-
-            .field {
-                width: 80%;
-                margin: 10px 0;
-                padding: 10px;
-                border: 1px solid #c8c8c8;
-            }
-        }
-
     }
-    .create-card {
-        margin-top:30px;
+
+    /* fields */
+    .fields {
+        width: 100%;
+        margin-bottom: 10px;
+
+        .field {
+            width: 80%;
+            margin: 10px 0;
+            padding: 10px;
+            border: 1px solid #c8c8c8;
+        }
+    }
+
+    /* create card action */
+    .create-card-action {
+        margin-top: 30px;
+
         .desc {
             font-family: $font_2;
             text-align: center;
-            font-size:14px;
+            font-size: 14px;
             margin-bottom: 10px;
+        }
+    }
+
+    /* create card */
+    .create-card {
+        margin-top: 10px;
+        box-shadow: 1px 1px 15px rgba(0, 0, 0, 0.16);
+        background-color: white;
+        padding: 20px;
+
+        .disclaimer {
+            margin-top:10px;
+            font-size:10px;
+            text-align: center;
         }
     }
 }
