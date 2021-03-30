de7a687de44dab2bd4b0d14f9ea2559c8f2a63b4
Suhaib Khan <hello@saibbyweb.com>
2021-03-30 16:18:27 +0530

    variant categories search implemented, working

--- pages/admin-panel/variantCategories.vue
+++ pages/admin-panel/variantCategories.vue
@@ -2,19 +2,19 @@
 <div class="variant-categories crud">
     <!-- filters -->
     <div :class="{updating: showForm}" class="filters center">
-        <input class="search shadow" type="text" placeholder="Search for Variant Categories" />
-        <SelectBox :options="filters" v-model="selectedFilter" />
+        <input v-model="rawCriterion.search.term" class="search shadow" type="text" placeholder="Search for Variant Categories" />
     </div>
     <!-- list of fabrics -->
     <div :class="{updating: showForm}" class="list">
-        <List :list="list" :model="model" :headings="headings" custom_css="10% 40% 25% 25%" @documentFetched="documentFetched" />
+        <List :list="list" :model="model" :headings="headings" custom_css="10% 40% 25% 25%" @documentFetched="documentFetched" :sortByFields="sortByFields" @sortToggled="sortToggled" />
+        <Pagination ref="pagination" :model="model" :rawCriterion="rawCriterion" @resultsFetched="resultsFetched" />
     </div>
     <!-- update fabrics form -->
     <div :class="{updating: showForm}" class="update">
-    
-        <UpdateVariantCategory v-show="showForm" ref="updateComponent" @updated="fetchList" :model="model" @close="showForm = false" />
 
-        <AddNewItem v-if="!showForm" label="Variant Category" @showForm="showForm = true"/>
+        <UpdateVariantCategory v-show="showForm" ref="updateComponent" @updated="updateList" :model="model" @close="showForm = false" />
+
+        <AddNewItem v-if="!showForm" label="Variant Category" @showForm="showForm = true" />
     </div>
 </div>
 </template>
@@ -27,42 +27,65 @@ export default {
             showForm: false,
             loading: false,
             model: 'variant_categories',
-            filters: [{
-                name: 'All Variant Categories',
-                value: 'all'
-            }, {
-                name: "Karakul",
-                value: 'karakul'
-            }],
-            selectedFilter: 'all',
             list: [],
-            headings: ['_id','Category Name', 'Description', 'Status']
+            /* rawCriterion */
+            rawCriterion: {
+                search: {
+                    key: "name",
+                    term: ""
+                },
+                filters: {
+                    type: 'default'
+                },
+                sortBy: {
+
+                },
+                limit: 2
+            },
+            sortByFields: ['name', 'status'],
+            headings: ['_id', 'name', 'Description', 'status']
         }
     },
     mounted() {
-        this.fetchList();
+        // this.fetchList();
     },
     methods: {
+        updateList() {
+            this.$refs.pagination.fetchResults();
+        },
+        sortToggled(sortBy) {
+            console.log(sortBy);
+            this.rawCriterion = {
+                ...this.rawCriterion,
+                sortBy
+            }
+        },
         documentFetched(doc) {
             this.showForm = true;
             this.editMode = true;
             console.log(this.$refs.updateComponent.populateForm(doc));
         },
-        async fetchList() {
-            this.loading = true;
-            const result = await this.$fetchCollection(this.model);
-            this.loading = false;
-
-            if(!result.fetched || result.docs.length === 0) {
+        async resultsFetched(result) {
+            if (result.docs.length === 0) {
                 this.list = [];
                 return;
             }
 
             /* extract list */
-            this.list = result.docs.map(({_id, name, description, status}) => {
-                return {_id, name, description, status }
+            this.list = result.docs.map(({
+                _id,
+                name,
+                description,
+                status
+            }) => {
+                return {
+                    _id,
+                    name,
+                    description,
+                    status
+                }
             });
-        
+
         }
     }
 }
