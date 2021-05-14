7bcb86292f131435c3701aa492c78343dcab75f7
Suhaib Khan <hello@saibbyweb.com>
2021-05-15 01:36:28 +0530

    new homepage admin panel page added

---
+++ pages/admin-panel/homepage.vue
@@ -0,0 +1,92 @@
+<template>
+<div class="homepages crud">
+    <!-- filters -->
+    <div :class="{updating: showForm}" class="filters center">
+        <input v-model="rawCriterion.search.term" class="search shadow" type="text" placeholder="Search for Homepages" />
+    </div>
+    <!-- list of homepage layouts -->
+    <div :class="{updating: showForm}" class="list">
+        <List :list="list" :model="model" :headings="headings" custom_css="10% 60% 20% 10%" :sortByFields="sortByFields" @documentFetched="documentFetched" @sortToggled="sortToggled" />
+
+        <Pagination ref="pagination" :model="model" :rawCriterion="rawCriterion" @resultsFetched="resultsFetched" />
+    </div>
+    <!-- update homepage layouts form -->
+    <div :class="{updating: showForm}" class="update">
+        <UpdateHomepageLayout v-show="showForm" ref="updateComponent" @updated="updateList" :model="model" @close="showForm = false" />
+
+        <AddNewItem v-if="!showForm" label="Homepage Layout" @showForm="showForm = true" />
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            showForm: false,
+            loading: false,
+            model: 'homepages',
+            /* raw criterion */
+            rawCriterion: {
+                search: {
+                    key: "name",
+                    term: ""
+                },
+                filters: {
+                    type: 'default'
+                },
+                sortBy: {
+
+                },
+                limit: 20
+            },
+            list: [],
+            sortByFields: ['name', 'status'],
+            headings: ['_id', 'name', 'description', 'status'],
+        }
+    },
+    methods: {
+        updateList() {
+            this.$refs.pagination.fetchResults();
+        },
+        sortToggled(sortBy) {
+            console.log(sortBy);
+            this.rawCriterion = {
+                ...this.rawCriterion,
+                sortBy
+            }
+        },
+        documentFetched(doc) {
+            this.showForm = true;
+            this.editMode = true;
+            /* add unique key to alternate sections */
+            this.$refs.updateComponent.populateForm(doc);
+            setTimeout(() => this.populateForm(doc), 1200);
+        },
+        populateForm(doc) {
+            /* do some magic here */
+        },
+        resultsFetched(result) {
+            if (result.docs.length === 0) {
+                this.list = [];
+                return;
+            }
+
+            /* extract list */
+            this.list = result.docs.map(({
+                _id,
+                name,
+                description,
+                status
+            }) => {
+                return {
+                    _id,
+                    name,
+                    description,
+                    status
+                }
+            });
+        }
+    }
+}
+</script>
