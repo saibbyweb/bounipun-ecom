f8097f80f88361ad127248be67714ff5825a06f7
Suhaib Khan <hello@saibbyweb.com>
2021-06-29 13:36:59 +0530

    global config db working

--- pages/admin-panel/globalConfig.vue
+++ pages/admin-panel/globalConfig.vue
@@ -2,39 +2,88 @@
   <div class="global-config">
     <!-- domestic shipping charge -->
     <InputBox
+      type="number"
       label="Domestic Shipping Charge"
       v-model="doc.domesticShippingCharge"
     />
     <!-- international shipping charge -->
     <InputBox
+      type="number"
       label="International Shipping Charge"
       v-model="doc.internationalShippingCharge"
     />
     <!-- gst percentage -->
-    <InputBox label="GST Percentage" v-model="doc.gstPercentage" />
+
+    <InputBox
+      type="number"
+      label="GST Percentage"
+      v-model="doc.gstPercentage"
+    />
     <!-- international tax percentage -->
     <InputBox
+      type="number"
       label="International Tax Percentage"
       v-model="doc.internationalTaxPercentage"
     />
-
-    <!-- update document -->
-    <button @click="updateDocument" class="action" :disabled="loading">
-      {{ editMode ? "Edit" : "Add" }} Coupon
-    </button>
+    
+    <!-- actions -->
+    <div class="actions">
+      <!-- action complete gif -->
+      <img v-if="updated" class="action-complete" src="/complete.gif" />
+      <!-- update document -->
+      <button @click="updateDocument" class="action">
+        Update Global Config
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  layout: "admin",
+  mounted() {
+    this.fetchConfig();
+  },
   data() {
     return {
+      model: "globalConfig",
       doc: {
+        _id: "",
         domesticShippingCharge: "",
         internationalShippingCharge: "",
         gstPercentage: "",
         internationalTaxPercentage: ""
-      }
+      },
+      loading: true,
+      updated: false
+    };
+  },
+  methods: {
+    async fetchConfig() {
+      const data = await this.$fetchData(this.model, {
+        bounipun_id: "saibbyweb"
+      });
+
+      if (data.fetched !== true) return;
+
+      const { doc } = data;
+
+      this.doc = {
+        _id: doc._id,
+        domesticShippingCharge: doc.domesticShippingCharge,
+        internationalShippingCharge: doc.internationalShippingCharge,
+        gstPercentage: doc.gstPercentage,
+        internationalTaxPercentage: doc.internationalTaxPercentage
+      };
+    },
+    async updateDocument() {
+  
+      const result = await this.$updateDocument(this.model, this.doc, true);
+
+
+      if (!result.updated) return;
+
+      this.$flash(this);
     }
   }
 };
