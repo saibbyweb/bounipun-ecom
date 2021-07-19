d8a5c7324babe86387032d6037a3e4a6ab8e46cf
Suhaib Khan <hello@saibbyweb.com>
2021-07-19 13:15:24 +0530

    terms re-write in process

--- pages/Terms.vue
+++ pages/Terms.vue
@@ -1,112 +1,107 @@
 <template>
-<div class="page article">
-    <h1> Terms Of Use </h1>
-    <p> Welcome to bounipun.in!
+  <div class="page">
+    <div class="wrapper">
+      <h1>Terms and Conditions</h1>
 
-        These terms and conditions outline the rules and regulations for the use of bounipun's Website, located at https://www.bounipun.in.
+      <!-- Covid19 Info -->
+      <Accordion heading="Covid19 Info">
+        Staying in line with the current lockdown in various parts of the
+        country, there is a delay in delivering orders as most of our courier
+        partners have halted operations while others are short staffed. We are
+        constantly monitoring the situation and working with our courier
+        partners to get your orders delivered as soon as possible. We appreciate
+        your patience and understanding!
+      </Accordion>
 
-        By accessing this website we assume you accept these terms and conditions. Do not continue to use bounipun.in if you do not agree to take all of the terms and conditions stated on this page. Our Terms and Conditions were created with the help of the Terms And Conditions Generator and the Free Terms & Conditions Generator.
+      <!-- product prices  -->
+      <Accordion heading="Product Prices">
+        All references to (INR) Rupee on this website are in Indian currency.
+        Indian prices are different to International prices due to taxes/duties
+        , and the laws of the countries they have been bought from. In cases of
+        International shipping , prices may vary when logging into the site from
+        different postal codes and locations. Prices are subject to change at
+        any time , without any notifications. All rights to change prices are
+        reserved by Bounipun Kashmir. Prices of products available at retail
+        outlets/ boutiques / resellers are only available within India. No
+        product can be sold for Indian prices outside the country.
+      </Accordion>
 
-        The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: "Client", "You" and "Your" refers to you, the person log on this website and compliant to the Company’s terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client’s needs in respect of provision of the Company’s stated services, in accordance with and subject to, prevailing law of India. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same. </p>
+      <!-- shipping and delivery -->
+      <Accordion heading="Shipping and Delivery">
+        Shipping costs are calculated based on cumulative weight of entire order
+        and are different for domestic and international delivery. In the event
+        that a non-delivery occurs on account of a mistake by you (i.e. wrong
+        name, address or contact number ) any extra cost towards re-delivery
+        shall be claimed by the user placing the order. Shipment/delivery time,
+        of the order processing starts from the day of receipt of the payment
+        confirmed against the order placed with Bounipun Kashmir. The shipment
+        /delivery time is an approximate time mentioned by the vendor against
+        each product. Bounipun Kashmir shall not be liable for any delay /
+        non-delivery of purchased goods by flood, fire, wars, acts of God or any
+        cause that is beyond the control of Bounipun Kashmir. The user must agree to
+        provide authentic and true information, to avoid any kind of delay or
+        loss of merchandise. Bounipun Kashmir reserves the right to confirm and
+        validate the information and other details provided by the user at any
+        point of time. If upon confirmation, such user details are found not
+        true (wholly or partly), Bounipun Kashmir has the right in its sole discretion
+        to reject the registration and debar the user from using the services
+        available at this website, and / or other affiliated websites without
+        prior intimation whatsoever. Delivery of orders would be done address
+        specific not person specific. Octroi charges may be levied on few
+        destinations (mostly in Maharashtra) displayed on the site .These
+        charges are not refundable and shall be borne by the customer. In the
+        event of cancellation of the order the Octroi charges levied shall not
+        be refunded. All Duties and taxes for international shipping shall be
+        borne by the customer. Bounipun Kashmir is not liable to pay any
+        Duties or Taxes for International Shipping.
+      </Accordion>
 
-    <h2> Cookies </h2>
-            <p> We employ the use of cookies. By accessing bounipun.in, you agreed to use cookies in agreement with the bounipun's Privacy Policy.
-
-                Most interactive websites use cookies to let us retrieve the user’s details for each visit. Cookies are used by our website to enable the functionality of certain areas to make it easier for people visiting our website. Some of our affiliate/advertising partners may also use cookies. </p>
-
-            <h2> License </h2>
-            <p>Unless otherwise stated, bounipun and/or its licensors own the intellectual property rights for all material on bounipun.in. All intellectual property rights are reserved. You may access this from bounipun.in for your own personal use subjected to restrictions set in these terms and conditions.
-
-                You must not:
-
-                Republish material from bounipun.in
-                Sell, rent or sub-license material from bounipun.in
-                Reproduce, duplicate or copy material from bounipun.in
-                Redistribute content from bounipun.in
-                This Agreement shall begin on the date hereof.
-
-                Parts of this website offer an opportunity for users to post and exchange opinions and information in certain areas of the website. bounipun does not filter, edit, publish or review Comments prior to their presence on the website. Comments do not reflect the views and opinions of bounipun,its agents and/or affiliates. Comments reflect the views and opinions of the person who post their views and opinions. To the extent permitted by applicable laws, bounipun shall not be liable for the Comments or for any liability, damages or expenses caused and/or suffered as a result of any use of and/or posting of and/or appearance of the Comments on this website.
-
-                bounipun reserves the right to monitor all Comments and to remove any Comments which can be considered inappropriate, offensive or causes breach of these Terms and Conditions.
-
-                You warrant and represent that:
-
-                You are entitled to post the Comments on our website and have all necessary licenses and consents to do so;
-                The Comments do not invade any intellectual property right, including without limitation copyright, patent or trademark of any third party;
-                The Comments do not contain any defamatory, libelous, offensive, indecent or otherwise unlawful material which is an invasion of privacy
-                The Comments will not be used to solicit or promote business or custom or present commercial activities or unlawful activity.
-                You hereby grant bounipun a non-exclusive license to use, reproduce, edit and authorize others to use, reproduce and edit any of your Comments in any and all forms, formats or media. </p>
-
-            <h2> Hyperlinking to our Content </h2>
-            <p> The following organizations may link to our Website without prior written approval:
-
-                Government agencies;
-                Search engines;
-                News organizations;
-                Online directory distributors may link to our Website in the same manner as they hyperlink to the Websites of other listed businesses; and
-                System wide Accredited Businesses except soliciting non-profit organizations, charity shopping malls, and charity fundraising groups which may not hyperlink to our Web site.
-                These organizations may link to our home page, to publications or to other Website information so long as the link: (a) is not in any way deceptive; (b) does not falsely imply sponsorship, endorsement or approval of the linking party and its products and/or services; and (c) fits within the context of the linking party’s site.
-
-                We may consider and approve other link requests from the following types of organizations:
-
-                commonly-known consumer and/or business information sources;
-                dot.com community sites;
-                associations or other groups representing charities;
-                online directory distributors;
-                internet portals;
-                accounting, law and consulting firms; and
-                educational institutions and trade associations.
-                We will approve link requests from these organizations if we decide that: (a) the link would not make us look unfavorably to ourselves or to our accredited businesses; (b) the organization does not have any negative records with us; (c) the benefit to us from the visibility of the hyperlink compensates the absence of bounipun; and (d) the link is in the context of general resource information.
-
-                These organizations may link to our home page so long as the link: (a) is not in any way deceptive; (b) does not falsely imply sponsorship, endorsement or approval of the linking party and its products or services; and (c) fits within the context of the linking party’s site.
-
-                If you are one of the organizations listed in paragraph 2 above and are interested in linking to our website, you must inform us by sending an e-mail to bounipun. Please include your name, your organization name, contact information as well as the URL of your site, a list of any URLs from which you intend to link to our Website, and a list of the URLs on our site to which you would like to link. Wait 2-3 weeks for a response.
-
-                Approved organizations may hyperlink to our Website as follows:
-
-                By use of our corporate name; or
-                By use of the uniform resource locator being linked to; or
-                By use of any other description of our Website being linked to that makes sense within the context and format of content on the linking party’s site.
-                No use of bounipun's logo or other artwork will be allowed for linking absent a trademark license agreement. </p>
-
-            <h2> iFrames </h2>
-            <p> Without prior approval and written permission, you may not create frames around our Webpages that alter in any way the visual presentation or appearance of our Website. </p>
-
-            <h2> Content Liability </h2>
-            <p> We shall not be hold responsible for any content that appears on your Website. You agree to protect and defend us against all claims that is rising on your Website. No link(s) should appear on any Website that may be interpreted as libelous, obscene or criminal, or which infringes, otherwise violates, or advocates the infringement or other violation of, any third party rights. </p>
-
-            <h2> Reservation of Rights </h2>
-            <p> We reserve the right to request that you remove all links or any particular link to our Website. You approve to immediately remove all links to our Website upon request. We also reserve the right to amen these terms and conditions and it’s linking policy at any time. By continuously linking to our Website, you agree to be bound to and follow these linking terms and conditions.</p>
-
-            <h2> Removal of links from our website </h2>
-            <p> If you find any link on our Website that is offensive for any reason, you are free to contact and inform us any moment. We will consider requests to remove links but we are not obligated to or so or to respond to you directly.
-
-                We do not ensure that the information on this website is correct, we do not warrant its completeness or accuracy; nor do we promise to ensure that the website remains available or that the material on the website is kept up to date. </p>
-
-            <h2> Disclaimer </h2>
-            <p> To the maximum extent permitted by applicable law, we exclude all representations, warranties and conditions relating to our website and the use of this website. Nothing in this disclaimer will:
-
-                limit or exclude our or your liability for death or personal injury;
-                limit or exclude our or your liability for fraud or fraudulent misrepresentation;
-                limit any of our or your liabilities in any way that is not permitted under applicable law; or
-                exclude any of our or your liabilities that may not be excluded under applicable law.
-                The limitations and prohibitions of liability set in this Section and elsewhere in this disclaimer: (a) are subject to the preceding paragraph; and (b) govern all liabilities arising under the disclaimer, including liabilities arising in contract, in tort and for breach of statutory duty.
-
-                As long as the website and the information and services on the website are provided free of charge, we will not be liable for any loss or damage of any nature. </p>
-</div>
+      <!-- Returns and exchanges -->
+      <Accordion heading="Returns and Exchanges">
+        Bounipun acknowledges return of products only limited to direct sale
+        between vendor and consumer. This only entails sale online, and not
+        through retail outlets. Any product that has been hampered or damaged
+        while transportation MUST be notified by email within 48 hours of the
+        product being delivered and returned to our warehouse within a strict
+        time period of one week, else Bounipun Kashmir will not be liable to
+        accept the merchandise. In such cases, the customer will have to write
+        in to care@bounipun.in and a prompt response is guaranteed from our end.
+        We will investigate the damage from our end and repair the merchandise
+        in entirety. ALL couriers of this nature will be borne by the customer
+        themselves. Bounipun will only bear the expenses of the transaction
+        after the repair of the merchandise. In cases wherein due to usage of
+        the merchandise, some portions of the product is damaged, such as
+        fabric, embroidery will only be repaired within a period of two weeks
+        from the time of receipt of the merchandise after dispatch from our
+        warehouse. Please note, damaged products specific to repair will have to
+        be couriered back to our warehouse wherein all costs will be borne by
+        the customer themselves. Bounipun Kashmir will only bear the expenses of
+        the transaction after the repair of the merchandise.
+      </Accordion>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-    .article {
-        padding:20px;
-        h1, h2 {
-            text-transform: uppercase;
-            font-family: $font_1;
-            margin:10px 0;
-        }
-        p {
-            font-family: $font_2;
-            font-size:14px;
-        }
-    }
+.article {
+  padding: 20px;
+
+  h1,
+  h2 {
+    text-transform: uppercase;
+    font-family: $font_1;
+    margin: 10px 0;
+  }
+
+  p {
+    font-family: $font_2;
+    font-size: 14px;
+  }
+}
+
+.wrapper {
+  h1 {
+    text-align: center;
+  }
+}
 </style>
