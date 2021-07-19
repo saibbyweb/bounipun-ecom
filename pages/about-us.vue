9b242eccb35e6d503867e5055271c31044c9cf95
Suhaib Khan <hello@saibbyweb.com>
2021-07-19 12:58:18 +0530

    about us page added

---
+++ pages/about-us.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="page">
+    <div class="wrapper flex center col">
+        <h1>About Bounipun</h1>
+        <br>
+        <p>
+            BOUNIPUN Cashmere Kashmir is a specialist luxury division lead by the
+            known internationally acclaimed fashion designer ZUBAIR KIRMANI who’s born
+            and brought up in the divine valley of Kashmir. <br /><br>
+
+            In fashion, he’s celebrated as a reformer of fabric, texture and
+            simplicity of silhouette. He was awarded as the “best debut” and
+            “neo-minimal designer” by ELLE magazine just after the launch of his men’s
+            and women’s label.<br /><br>
+
+            His profile has been published by an Italian Publication (Damiani) based
+            on CONTEMPORARY INDIAN FASHION.<br /><br>
+
+            Winner of the prestigious International Woolmark Prize 2016/17 India,
+            Pakistan and Middle East regional final for his innovative development in
+            wool held in Paris.<br /><br>
+
+            And now, after winning laurels outside, he couldn’t resist going back to
+            his roots, his own Kasheer, with plans to not only showcase the exotic
+            Kashmiri craft using traditional but as well as ultra-modern methods to
+            create some of the world’s finest wearable art CRAFTED IN KASHMIR that
+            literally has no peer.<br /><br>
+            But in the process, generating avenues of employment for thousands of
+            craftsmen and talented local youth. The vision is to uplift the local
+            craft by creating synthesis of folklore, ethnicity, hues and tints, most
+            importantly signature design that is omnipresent in KASHMIR.
+        </p>
+    </div>
+</div>
+</template>
