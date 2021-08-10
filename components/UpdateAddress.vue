527cc741122c73f1fcc61cafb5a9cec46853d82d
Suhaib Khan <hello@saibbyweb.com>
2021-08-10 12:37:13 +0530

    update address validation working

--- components/UpdateAddress.vue
+++ components/UpdateAddress.vue
@@ -13,7 +13,7 @@
     <!-- delivery input fields -->
 
     <DeliveryInput
-     v-for="(field, key, index) in formData"
+      v-for="(field, key, index) in formData"
       :key="index"
       v-model="field.value"
       :error="field.error"
@@ -24,7 +24,7 @@
       :countryCode="addressDetails.countryDialCode"
       :disabled="false"
     />
- 
+
     <br />
     <div class="actions center">
       <!-- add or update address -->
@@ -46,6 +46,7 @@
 </template>
 
 <script>
+import validate from "@/helpers/validate.js";
 export default {
   props: {
     updating: {
@@ -115,14 +116,14 @@ export default {
       this.deliveryAddress = this.addressDetails;
     },
     async updateAddress() {
-
+      if (!this.validateForm()) return;
 
       /* collect delivery address */
       let deliveryAddress = {};
       Object.keys(this.formData).forEach(key => {
         deliveryAddress[key] = this.formData[key].value;
       });
-      
+
       deliveryAddress._id = this.addressDetails._id;
       deliveryAddress.countryDialCode = this.addressDetails.countryDialCode;
       deliveryAddress.countryIsoCode = this.addressDetails.countryIsoCode;
@@ -133,22 +134,117 @@ export default {
       });
       /* fetch profile */
       this.$store.dispatch("customer/fetchProfile");
+
+      if (!this.updating) {
+        this.$emit("goBack");
+        // window.scroll({ top: 0, behavior: "smooth" });
+      }
+
       this.updated = true;
       setTimeout(() => (this.updated = false), 3000);
     },
     async deleteAddress() {
-      const deliveryAddress = { _id: this.addressDetails._id }
+      const deliveryAddress = { _id: this.addressDetails._id };
 
       const deleteAddress = await this.$post("/addressBookActions", {
-        action: 'delete-address',
+        action: "delete-address",
         address: deliveryAddress
       });
 
-      this.$emit('goBack');
+      this.$emit("goBack");
       window.scroll({ top: 0, behavior: "smooth" });
       /* fetch profile */
       this.$store.dispatch("customer/fetchProfile");
-    }
+    },
+      setError(key, flag, msg) {
+      const field = this.formData[key];
+      if (flag) {
+        field.error = {
+          status: true,
+          msg
+        };
+      }
+    },
+     validateForm() {
+      /* required fields */
+      const requiredFields = [
+        "firstName",
+        "surName",
+        "mobileNumber",
+        "email",
+        "addressLine1",
+        "city",
+        "postalCode"
+      ];
+
+      /* firstName and surName should only consist of alphabets */
+      ["firstName", "surName"].forEach(key => {
+        const field = this.formData[key];
+        const hasOnlyAlphabets = field.value.hasOnlyAlphabets();
+        this.setError(key, !hasOnlyAlphabets, "Only Alphabets are allowed!");
+      });
+
+      /*  mobile number field */
+      const mobileNumberField = this.formData["mobileNumber"];
+
+      /* mobile number should only consist of numbers  */
+      const mobileNumberHasOnlyNumbers = mobileNumberField.value.hasOnlyNumbers();
+      this.setError(
+        "mobileNumber",
+        !mobileNumberHasOnlyNumbers,
+        "Mobile number can only consist of digits"
+      );
+
+      /* mobile number length should be between 4 - 14 */
+      const mobileNumberLengthInRange =
+        mobileNumberField.value.length > 3 &&
+        mobileNumberField.value.length < 15;
+      this.setError(
+        "mobileNumber",
+        !mobileNumberLengthInRange,
+        "Please enter a valid mobile number"
+      );
+
+      /* email should be in a valid format */
+      const emailField = this.formData["email"];
+      const emailValid = emailField.value.isValidEmail();
+      this.setError("email", !emailValid, "Please enter a valid email address");
+
+      /* city should only consist of alphabets */
+      const cityField = this.formData["city"];
+      const cityOnlyHasAlphabets = cityField.value.hasOnlyAlphabets();
+      this.setError(
+        "city",
+        !cityOnlyHasAlphabets,
+        "Only Alphabets are allowed"
+      );
+
+      /* postal code should only consist of numbers */
+      const postalCodeField = this.formData["postalCode"];
+      const postalCodeOnlyHasNumbers = postalCodeField.value.hasOnlyNumbers();
+      this.setError(
+        "postalCode",
+        !postalCodeOnlyHasNumbers,
+        "Only Number are allowed"
+      );
+
+      /* except for addressLine#2, no field can be blank */
+      requiredFields.forEach(key => {
+        const field = this.formData[key];
+        const fieldEmpty = field.value.isEmpty();
+        this.setError(key, fieldEmpty, "This field cannot be left blank!");
+      });
+
+      /* check for any error flags */
+      const validated = requiredFields.every(key => {
+        console.log(this.formData[key].error.status, key);
+        return !this.formData[key].error.status;
+      });
+
+      console.log(validated, "--validated");
+
+      return validated;
+    },
   }
 };
 </script>
@@ -207,19 +303,18 @@ export default {
   }
 
   .actions {
-      .action {
-          margin: 0 8px;
-          &.remove {
-              background-color: rgb(157, 36, 36);
-              
-          }
+    .action {
+      margin: 0 8px;
+      &.remove {
+        background-color: rgb(157, 36, 36);
       }
+    }
 
-      @media(max-width: 768px) {
-          .action {
-            font-size: 11px;
-          }
+    @media (max-width: 768px) {
+      .action {
+        font-size: 11px;
       }
+    }
   }
 }
 .response {
