84a9c9dc1e4ad9b109669b675d5eec88d9c684bc
Suhaib Khan <hello@saibbyweb.com>
2021-10-06 21:49:27 +0530

    release 6 oct

---
+++ dist/frontend/_nuxt/993ed56.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[38],{861:function(n,e,t){"use strict";t.r(e);var l=t(9),component=Object(l.a)({},(function(){var n=this.$createElement;return(this._self._c||n)("div")}),[],!1,null,null,null);e.default=component.exports}}]);
