a6ea60054da3c8a666d1363c004462ed0a60ad8d
Suhaib Khan <hello@saibbyweb.com>
2021-10-24 16:40:43 +0530

    out story page initialized

---
+++ pages/our-story.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="page">
+    <!-- header image with arrow in bottom-middle part -->
+    <div class="header-image"></div>
+    <!-- centralized text block -->
+    <div class="c-text">
+      <p></p>
+    </div>
+    <!-- section 1 (45/55) - (first one with image + text, second one with just a long image) -->
+    <div class="section-split">
+      <!-- first part -->
+      <div class="one">
+        <!-- one image -->
+        <div class="one-image"></div>
+        <!-- text -->
+        <div class="text"></div>
+      </div>
+      <!-- second part -->
+      <div class="two">
+        <p></p>
+      </div>
+    </div>
+    <!-- full width image -->
+    <div class="full-width"></div>
+    <!-- centralized text block -->
+    <div class="c-text">
+      <p></p>
+    </div>
+    <!-- reverse of section 1 -->
+    <div class="section-split reverse">
+      <!-- first part -->
+      <div class="one">
+        <!-- one image -->
+        <div class="one-image"></div>
+        <!-- text -->
+        <div class="text"></div>
+      </div>
+      <!-- second part -->
+      <div class="two">
+        <p></p>
+      </div>
+    </div>
+    <!-- full width image -->
+    <div class="full-width"></div>
+    <!-- centralized text block -->
+    <div class="c-text">
+      <p></p>
+    </div>
+    <!-- bordered block with image and text -->
+    <div class="bordered-block"></div>
+    <!-- reverse of bordered block with image and text -->
+    <div class="bordered-block reverse"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "Our Story | Bounipun Kashmir",
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
