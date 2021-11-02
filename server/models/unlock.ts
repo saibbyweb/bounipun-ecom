66364fbbaf9262593b9ddf5d6013b9a11b70c11e
Suhaib Khan <hello@saibbyweb.com>
2021-11-02 14:08:42 +0530

    validity function added for unlock codes, untested

--- server/models/unlock.ts
+++ server/models/unlock.ts
@@ -1,4 +1,4 @@
-import { mongoose, ObjectId } from "@helpers/essentials";
+import { mongoose, ObjectId, task } from "@helpers/essentials";
 
 /* schema */
 const schema = new mongoose.Schema({
@@ -37,6 +37,41 @@ export const methods = {
     },
     /* check for expiry and black list  */
     async validateUnlockCode(code, user) {
+        const checkValidity: any = model.findOne({
+            code: code.toUpperCase(),
+            status: true,
+            validity: { $gt: 0 },
+            "validityRange.start": { $lte: new Date() },
+            "validityRange.end": { $gte: new Date().setHours(0, 0, 0, 0) }
+        }).select('code blackList log');
+
+        /* check validity */
+        const { response: codeDoc, error } = await task(checkValidity);
+
+        /* if error occured or code not valid */
+        if (error || codeDoc === null) {
+            console.log('Unlock Code Invalid');
+            return false;
+        }
+
+        /* coupon is valid, check if user is in black list*/
+        const blackListIndex = codeDoc.blackList.findIndex(u => u.toString() === user.toString())
+
+        if (blackListIndex !== -1) {
+            console.log('User found in black list');
+            return false;
+        }
+
+        /* check if user already in log (prevent duplicate usage) */
+        const logIndex = codeDoc.log.findIndex(log => log.user.toString() === user.toString())
+
+        if (logIndex !== -1) {
+            console.log('User already in log');
+            return false;
+        }
+
+        console.log(codeDoc);
+        return codeDoc;
 
     },
     async updateUnlockCodeLog(code, user) {
