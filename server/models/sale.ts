af8e090947f6b8ad57969271a96d6a7e21712446
Suhaib Khan <hello@saibbyweb.com>
2021-11-26 15:33:08 +0530

    sale in process

--- server/models/sale.ts
+++ server/models/sale.ts
@@ -1,4 +1,4 @@
-import { mongoose, ObjectId, task } from "@helpers/essentials";
+import { mongoose, ObjectId, db, task } from "@helpers/essentials";
 
 /* schema */
 const schema = new mongoose.Schema(
@@ -6,6 +6,10 @@ const schema = new mongoose.Schema(
     name: String,
     list: { type: ObjectId, ref: "product_lists" },
     discountPercentage: Number,
+    validityRange: {
+      start: Date,
+      end: Date,
+    },
     description: String,
     status: Boolean,
   },
@@ -22,40 +26,152 @@ export const methods = {
   register() {
     console.log("registered");
   },
-  alreadyInSalesList(productId) {},
-  normalizePricing(products) {
-    let normalizedProducts;
-    let saleDetails = {};
+  async validateSale(saleId) {
+    const checkValidity: any = model
+      .findOne({
+        _id: saleId,
+        status: true,
+        "validityRange.start": { $lte: new Date() },
+        "validityRange.end": { $gte: new Date().setHours(0, 0, 0, 0) },
+      })
+      .select("name list discountPercentage");
 
+    /* check validity */
+    const { response: saleDoc, error } = await task(checkValidity);
+
+    /* if error occured or code not valid */
+    if (error || saleDoc === null) {
+      console.log("Sale Invalid");
+      return false;
+    }
+
+    return saleDoc;
+  },
+  savePrices(INRPrice, NonINRPricing, allINRPrices, allNonINRPrices) {
+    allINRPrices.push(INRPrice);
+    if (NonINRPricing === undefined) return;
+    /* get all currency codes */
+    const currencyCodes = Object.keys(NonINRPricing);
+    /* save pricing for all non-inr currencies */
+    for (const code of currencyCodes) {
+      if (allNonINRPrices[code] === undefined) allNonINRPrices[code] = [];
+      allNonINRPrices[code].push(NonINRPricing[code]);
+    }
+
+    return { allINRPrices, allNonINRPrices };
+  },
+  async normalizePricing(products) {
+    /* list of products along with adjusted pricing */
+    let normalizedProducts = [];
+    /* hashmap of all valid sale details fetched during this routine */
+    let validSales = {};
+    /* hashmao of all invalid sales (with value = true) */
+    let invalidSales = {};
+
+    /* loop through every product */
     for (const product of products) {
       /* if product is not under sale, save product as is */
-      if (product.sale === null || product.sale == undefined) {
+      if (
+        product.sale === null ||
+        product.sale == undefined ||
+        invalidSales[product.sale]
+      ) {
         normalizedProducts.push(product);
+        continue;
       }
 
-      /* if product is under sale, check whether sale details are already available or not */
-      if (saleDetails[product.sale] === undefined) {
-        // fetch sale details and store it in saleDetails array
-        // saleDetails[product.sale] = saleDetailsFetched;
+      /* if product is under a sale, check whether sale details are already available or not */
+      if (validSales[product.sale] === undefined) {
+        /* fetch sale details */
+        const saleDetails = await this.validateSale(product.sale);
+        /* (if sale is invalid, store invalid sale details, and save product as is) */
+        if (saleDetails === false) {
+          invalidSales[product.sale] = true;
+          normalizedProducts.push(product);
+          continue;
+        }
+        /* if sale is valid, store it in valid sales hashmap */
+        validSales[product.sale] = saleDetails;
       }
 
-      /* extract variants and availability type */
-      const { variants, availabilityType } = product;
+      /* extract discount percentage */
+      const { discountPercentage } = validSales[product.sale];
+
+      /* extract variants, availability type, directPrice, directPricing */
+      let { variants, availabilityType, directPrice, directPricing } = product;
+
+      /* all prices and non-inr prices array [for calculating range] */
+      let allINRPrices = [];
+      let allNonINRPrices = {};
 
       if (availabilityType === "made-to-order") {
         variants.forEach(({ fabrics }) => {
           fabrics.forEach((fabric) => {
-            // do the magic, call function along with sale details and get updated values
+            /* calculate discount prices */
+            const { INRPrice, NonINRPricing } = this.getDiscountedPrices(
+              fabric.price,
+              fabric.pricing,
+              discountPercentage
+            );
+            /* set discounted prices */
+            fabric.price = INRPrice;
+            fabric.pricing = NonINRPricing;
+
+            /* save all prices in a one-dimensional arrays */
+            const updatedValues = this.savePrices(
+              INRPrice,
+              NonINRPricing,
+              allINRPrices,
+              allNonINRPrices
+            );
+            allINRPrices = updatedValues.allINRPrices;
+            allNonINRPrices = updatedValues.allNonINRPrices;
           });
         });
+
+        /* calculate inr price range and non-inr pricing range */
       } else {
-        // update direct price and direct pricing
-        // call function along with sale details and get updated values
+        const { INRPrice, NonINRPricing } = this.getDiscountPrices(
+          directPrice,
+          directPricing,
+          discountPercentage
+        );
+        /* update direct price and direct pricing */
+        directPrice = INRPrice;
+        directPricing = NonINRPricing;
+
+        /* save all prices in a one-dimensional arrays */
+        const updatedValues = this.savePrices(
+          INRPrice,
+          NonINRPricing,
+          allINRPrices,
+          allNonINRPrices
+        );
+        allINRPrices = updatedValues.allINRPrices;
+        allNonINRPrices = updatedValues.allNonINRPrices;
       }
 
-      // now re-calculate pricing range (make it a separate function)
+      /* recalculate inr price range */
+      product.priceRange = {
+        startsAt: Math.min(...allINRPrices),
+        endsAt: Math.max(...allINRPrices),
+      };
 
-      // save product with new values in normalized products array
+      /* recalculate non-inr pricing range */
+      const currencyCodes = Object.keys(allNonINRPrices);
+      if (currencyCodes.length !== 0) {
+        let pricingRange = {};
+
+        for (const code of currencyCodes) {
+          pricingRange[code] = {
+            startsAt: Math.min(...nonINRPrices[code]),
+            endsAt: Math.max(...nonINRPrices[code]),
+          };
+        }
+        product.pricingRange = pricingRange;
+      }
+      /* save product with new values in normalized products array */
+      normalizedProducts.push(product);
     }
   },
 };
