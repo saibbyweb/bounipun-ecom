48f6e4096309a4f8bc4116f2885e2f6e64201a41
Suhaib Khan <hello@saibbyweb.com>
2021-11-27 14:34:34 +0530

    validity range calendar picker added

--- components/admin/sales/UpdateSale.vue
+++ components/admin/sales/UpdateSale.vue
@@ -1,15 +1,36 @@
 <template>
-<div class="contents">
+  <div class="contents">
     <CancelUpdate @close="closeForm" />
-    <h2 class="heading"> {{ editMode ? 'Update' : 'Add New' }} Sale </h2>
+    <h2 class="heading">{{ editMode ? "Update" : "Add New" }} Sale</h2>
     <!-- sale ID -->
-    <InputBox v-if="editMode" label="Sale ID" v-model="doc._id" disabled :internal="true" />
+    <InputBox
+      v-if="editMode"
+      label="Sale ID"
+      v-model="doc._id"
+      disabled
+      :internal="true"
+    />
     <!-- sale name -->
     <InputBox label="Sale Name" v-model="doc.name" />
     <!-- product list -->
-    <SelectBox :options="allProductLists" v-model="doc.list" label="Select Product List" />
+    <SelectBox
+      :options="allProductLists"
+      v-model="doc.list"
+      label="Select Product List"
+    />
+
+    <!-- validity range -->
+    <label class="label"> Validity Range </label>
+    <client-only>
+      <v-datepicker
+        color="blue"
+        is-range
+        v-model="doc.validityRange"
+        :from-date="doc.validityRange.start"
+      />
+    </client-only>
+
     <!-- discount percentage -->
-    <!-- <InputBox label="Discount Percentage" v-model="doc.discountPercentage" type="number" /> -->
     <InputSlider label="Discount Percentage" v-model="doc.discountPercentage" />
     <!-- description -->
     <TextBox v-model="doc.description" label="Description" :internal="true" />
@@ -17,141 +38,142 @@
     <Toggle v-model="doc.status" label="Status" />
     <!-- update button -->
     <div class="center-space">
-        <!-- loading bar -->
-        <img v-if="loading" class="loading" src="/loading.gif" />
-        <!-- action complete gif -->
-        <img v-if="updated" class="action-complete" src="/complete.gif" />
-        <!-- update document -->
-        <button @click="updateDocument" class="action" :disabled="loading"> {{ editMode ? "Edit" : "Add" }} Sale </button>
-        <!-- delete document -->
-        <button v-if="editMode" @click="deleteDocument" class="action delete" :disabled="loading"> Delete </button>
+      <!-- loading bar -->
+      <img v-if="loading" class="loading" src="/loading.gif" />
+      <!-- action complete gif -->
+      <img v-if="updated" class="action-complete" src="/complete.gif" />
+      <!-- update document -->
+      <button @click="updateDocument" class="action" :disabled="loading">
+        {{ editMode ? "Edit" : "Add" }} Sale
+      </button>
+      <!-- delete document -->
+      <button
+        v-if="editMode"
+        @click="deleteDocument"
+        class="action delete"
+        :disabled="loading"
+      >
+        Delete
+      </button>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
-
 export default {
-    props: {
-        model: String,
-    },
-    data() {
+  props: {
+    model: String,
+  },
+  data() {
+    return {
+      editMode: false,
+      doc: {
+        _id: "",
+        name: "",
+        list: "",
+        discountPercentage: 0,
+        validityRange: { start: new Date(), end: new Date() },
+        description: "",
+        status: false,
+      },
+      allProductLists: [],
+      loading: false,
+      updated: false,
+    };
+  },
+  mounted() {
+    this.fetchAllProductLists();
+  },
+  methods: {
+    async fetchAllProductLists() {
+      const result = await this.$fetchCollection("product_lists");
+      this.allProductLists = result.docs.map(({ _id, name }) => {
         return {
-            editMode: false,
-            doc: {
-                _id: "",
-                name: "",
-                list: "",
-                discountPercentage: 0,
-                description: "",
-                status: false
-            },
-            allProductLists: [],
-            loading: false,
-            updated: false
-        }
+          name,
+          value: _id,
+        };
+      });
+      this.allProductLists.unshift({
+        name: "Select Product List",
+        value: null,
+      });
     },
-    mounted() {
-        this.fetchAllProductLists();
+    async updateDocument() {
+      this.loading = true;
+      const result = await this.$updateDocument(
+        this.model,
+        this.doc,
+        this.editMode
+      );
+      this.loading = false;
+
+      if (!result.updated) return;
+
+      this.$emit("updated");
+      this.populateForm(result.doc);
+      this.$flash(this);
     },
-    methods: {
-        async fetchAllProductLists() {
-            const result = await this.$fetchCollection('product_lists');
-            this.allProductLists = result.docs.map(({
-                _id,
-                name
-            }) => {
-                return {
-                    name,
-                    value: _id
-                }
-            });
-            this.allProductLists.unshift({
-                name: 'Select Product List',
-                value: null
-            });
-        },
-        async updateDocument() {
+    async deleteDocument() {
+      this.loading = true;
+      const result = await this.$deleteDocument(this.model, this.doc._id);
+      this.loading = false;
 
-            this.loading = true;
-            const result = await this.$updateDocument(this.model, this.doc, this.editMode);
-            this.loading = false;
+      if (!result.deleted) return;
 
-            if (!result.updated)
-                return;
-
-            this.$emit('updated');
-            this.populateForm(result.doc);
-            this.$flash(this);
-        },
-        async deleteDocument() {
-            this.loading = true;
-            const result = await this.$deleteDocument(this.model, this.doc._id);
-            this.loading = false;
-
-            if (!result.deleted)
-                return;
-
-            this.$emit('updated');
-            this.resetForm();
-            this.$flash(this);
-        },
-        populateForm(details) {
-            const {
-                _id,
-                name,
-                list,
-                discountPercentage,
-                description,
-                status
-            } = details;
-            this.doc = {
-                _id,
-                name,
-                list,
-                discountPercentage: discountPercentage.toString(),
-                description,
-                status
-            };
-            this.editMode = true;
-        },
-        closeForm() {
-            this.resetForm();
-            this.$emit('close');
-        },
-        resetForm() {
-            this.populateForm({
-                _id: "",
-                name: "",
-                list: "",
-                discountPercentage: "",
-                description: "",
-                status: false
-            });
-            this.editMode = false;
-        }
-    }
-}
+      this.$emit("updated");
+      this.resetForm();
+      this.$flash(this);
+    },
+    populateForm(details) {
+      const { _id, name, list, discountPercentage, validityRange, description, status } =
+        details;
+      this.doc = {
+        _id,
+        name,
+        list,
+        validityRange: validityRange ? validityRange : { start: new Date(), end: new Date() },
+        discountPercentage: discountPercentage.toString(),
+        description,
+        status,
+      };
+      this.editMode = true;
+    },
+    closeForm() {
+      this.resetForm();
+      this.$emit("close");
+    },
+    resetForm() {
+      this.populateForm({
+        _id: "",
+        name: "",
+        list: "",
+        discountPercentage: "",
+        validityRange: { start: new Date(), end: new Date() },
+        description: "",
+        status: false,
+      });
+      this.editMode = false;
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .section {
-    position: relative;
-    ;
-    margin: 10px;
-    padding: 5px 5px 30px 5px;
-    border-radius: 5px;
-    overflow: hidden;
+  position: relative;
+  margin: 10px;
+  padding: 5px 5px 30px 5px;
+  border-radius: 5px;
+  overflow: hidden;
 }
 
 .label {
-    font-family: $font_2_bold;
-    color: $gray;
-    text-transform: uppercase;
-    font-size: 10px;
-    padding: 2%;
-    margin-left: 5px;
-    font-weight: 900;
+  font-family: $font_2_bold;
+  color: $gray;
+  text-transform: uppercase;
+  font-size: 10px;
+  padding: 2%;
+  margin-left: 5px;
+  font-weight: 900;
 }
-
 </style>
