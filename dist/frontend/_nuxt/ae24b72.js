3c7027b98f0153f5b8e590732b51de6af04b1332
Suhaib Khan <hello@saibbyweb.com>
2022-02-11 15:01:01 +0530

    release 11feb2022

--- dist/frontend/_nuxt/ae24b72.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[68],{636:function(n,e,t){"use strict";t.r(e);t(41);e.default={methods:{assignImages:function(n,image){var e=this;""!==image&&void 0!==image&&setTimeout((function(){e.$refs.updateComponent.$refs[n].assignImages([{_id:"",mainImage:!1,path:image}])}),1200)}}}}}]);
