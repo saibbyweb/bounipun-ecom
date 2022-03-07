9a5e87fbe4c421b140b51d053d5bb8a59e366989
Suhaib Khan <hello@saibbyweb.com>
2022-03-07 13:07:15 +0530

    pop up mongoose model created

---
+++ server/models/popup.ts
@@ -0,0 +1,29 @@
+import { mongoose } from "@helpers/essentials";
+
+/* schema */
+const schema = new mongoose.Schema(
+    {
+        name: String,
+        image: String,
+        category: { type: String, enum: ['normal', 'coupon'] },
+        text: String,
+        actionURL: String,
+        persist: { type: Boolean, default: false },
+        delay: { type: Number, default: 20 },
+        visibility: { type: String, enum: ['guests', 'registered-users', 'both'] },
+        status: { type: Boolean, default: false }
+    },
+    {
+        timestamps: true,
+    }
+);
+
+/* model */
+const model = mongoose.model("popup", schema);
+
+/* helper methods */
+export const methods = {
+}
+
+
+export default { model, methods };
