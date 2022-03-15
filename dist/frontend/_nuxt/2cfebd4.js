2ddf592e4a707b46172fff3b782f3735ff4c71da
Suhaib Khan <hello@saibbyweb.com>
2022-03-15 16:01:11 +0530

    popup release v1

---
+++ dist/frontend/_nuxt/2cfebd4.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[44],{937:function(n,e,t){"use strict";t.r(e);var l=t(9),component=Object(l.a)({},(function(){var n=this.$createElement;return(this._self._c||n)("div")}),[],!1,null,null,null);e.default=component.exports}}]);
