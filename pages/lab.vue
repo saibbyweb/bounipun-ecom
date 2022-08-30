eebc9d95b6484e8e462320cd1cb60ddb6156c253
Suhaib Khan <hello@saibbyweb.com>
2022-08-30 11:05:02 +0530

    bounipun lab page being connected to backend

--- pages/lab.vue
+++ pages/lab.vue
@@ -1,19 +1,27 @@
 <template>
-  <div class="page">
-      <!-- hero -->
-    <div class="hero">
+  <div class="page -wh">
+    <!-- hero -->
+    <div v-if="layoutFetched" class="hero flex center" :style="setHeroImage()">
+      <!-- content -->
+      <div class="content flex center col">
         <!-- title -->
-        <h1> {{ layout.title }} </h1>
+        <h1 class="title">{{ layout.title }}</h1>
         <!-- tagline -->
-        <p> {{ layout.tagline }} </p>
+        <p class="tagline">{{ layout.tagline }}</p>
 
         <!-- hero blocks -->
         <div class="hero-blocks">
-            <div class="hero-block" v-for="(heroBlock, index) in layout.heroBlocks" :key="index">
-                <h3> {{ heroBlock.name }} </h3>
-                <p> {{ heroBlock.paragraph }} </p>
-            </div>
+          <div
+            class="hero-block"
+            v-for="(heroBlock, index) in layout.heroBlocks"
+            :key="index"
+          >
+            <h3>{{ heroBlock.name }}</h3>
+            <p>{{ heroBlock.paragraph }}</p>
+          </div>
         </div>
+      </div>
+      <!-- content ** ends here -->
     </div>
   </div>
 </template>
@@ -23,6 +31,7 @@ export default {
   data() {
     return {
       layout: {},
+      layoutFetched: false,
     };
   },
   mounted() {
@@ -38,7 +47,38 @@ export default {
         return;
       }
       this.layout = layout.doc;
+      this.layoutFetched = true;
+    },
+    setHeroImage() {
+      return {
+        backgroundImage: `url(${this.$getOriginalPath(this.layout.heroImage)})`,
+      };
     },
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.page {
+}
+.hero {
+  min-height: 80vh;
+  background-size: cover;
+  background-repeat: no-repeat;
+
+  .content {
+    background-color: rgba(255, 255, 255, 0.881);
+    height: 65vh;
+    width: 88%;
+
+    .title {
+      font-family: $font_1_bold;
+      font-size: 42px;
+      letter-spacing: 1px;
+    }
+    .tagline {
+      font-family: $font_2;
+    }
+  }
+}
+</style>
