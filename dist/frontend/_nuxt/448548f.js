8a53adda1738eaa980c02aa9cffd2bdc8aac3f32
Suhaib Khan <hello@saibbyweb.com>
2022-09-01 14:29:14 +0530

    release september 1 - bounipun lab

--- dist/frontend/_nuxt/448548f.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[82],{732:function(n,e,t){"use strict";t.r(e);t(48);e.default={methods:{assignImages:function(n,image){var e=this;""!==image&&void 0!==image&&setTimeout((function(){e.$refs.updateComponent.$refs[n].assignImages([{_id:"",mainImage:!1,path:image}])}),1200)}}}}}]);
